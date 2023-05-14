ffd6d9b30f8cd94eee57f00f40f5a2fbef6d6bb6
romGki <gkiroma@gmail.com>
2023-05-14 12:19:50 +0300

    try

--- app/src/main/java/com/example/menu/JsonParser.java
+++
@@ -1,4 +0,0 @@
-package com.example.menu;
-
-public class JsonParser {
-}
--- app/src/main/java/com/example/menu/Supermarkets.java
+++ app/src/main/java/com/example/menu/Supermarkets.java
@@ -1,97 +1,41 @@
 package com.example.menu;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.location.Location;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.Spinner;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.menu.databinding.ActivitySupermarketsBinding;
 
-public class Supermarkets extends AppCompatActivity {
+public class Supermarkets extends FragmentActivity implements OnMapReadyCallback {
 
-    Spinner spType;
-    Button btFind;
-    SupportMapFragment supportMapFragment;
-    GoogleMap map;
-    FusedLocationProviderClient fusedLocationProviderClient;
-    double currentLat = 0, currentLong = 0;
+    private GoogleMap mMap;
+    private ActivitySupermarketsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_supermarkets);
 
-        spType = findViewById(R.id.sp_type);
-        btFind = findViewById(R.id.bt_find);
-        supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map);
+        binding = ActivitySupermarketsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        String[] placeTypeList = {"supermarket"};
-        String[] placeNameList = {"supermarket"};
-
-        spType.setAdapter(new ArrayAdapter<>(Supermarkets.this, android.R.layout.simple_spinner_dropdown_item, placeNameList));
-
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-
-        if (ActivityCompat.checkSelfPermission(Supermarkets.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            getCurrentLocation();
-        }else {
-            ActivityCompat.requestPermissions(Supermarkets.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
-        }
-
-        btFind.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int i = spType.getSelectedItemPosition();
-                String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json" +
-                        "?location=" + currentLat + "," + currentLong + "&radius=5000" + "&types="
-                        + placeTypeList[i] + "&sensor=true" + "&key=" + getResources().getString(R.string.google_map_key);
-            }
-        });
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
-
-    private  void getCurrentLocation() {
-
-        Task<Location> task = fusedLocationProviderClient.getLastLocation();
-        task.addOnSuccessListener(new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-                if (location != null){
-                    currentLat = location.getLatitude();
-                    currentLong = location.getLongitude();
-                    supportMapFragment.getMapAsync(new OnMapReadyCallback() {
-                        @Override
-                        public void onMapReady(@NonNull GoogleMap googleMap) {
-                            map = googleMap;
-                            map.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(currentLat, currentLong), 10));
-                        }
-                    });
-                }
-            }
-        })
-    }
-
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == 44){
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                getCurrentLocation();
-            }
-        }
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
