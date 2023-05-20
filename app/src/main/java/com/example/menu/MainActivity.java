31e2b07d40e8ba2e3bfe454340335afc6914a993
Eabky <chriskatsibas7@gmail.com>
2023-05-20 14:50:08 +0300

    Synced all the branches.

--- app/src/main/java/com/example/menu/MainActivity.java
+++ app/src/main/java/com/example/menu/MainActivity.java
@@ -19,4 +19,10 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, Supermarkets.class);
         startActivity(intent);
     }
+
+    public void startRecipesActivity(View view) {
+
+        Intent intent = new Intent(this, RecipesList.class);
+        startActivity(intent);
+    }
 }
